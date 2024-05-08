42a522e0a15ccaaf7b9ecdf61ac5e7327282e7d7
samanta <samantaschinca@gmail.com>
2024-05-08 16:06:20 -0300

    tp3

--- tp3/Codigo_funcionando.c
+++ tp3/Codigo_funcionando.c
@@ -11,22 +11,23 @@ void *comer_hamburguesa(void *tid)
 	while (1 == 1)
 	{ 
 		while(turno!=(int)tid){
+		
     // INICIO DE LA ZONA CRÍTICA
 		if (cantidad_restante_hamburguesas > 0)
 		{
-			printf("Hola! soy el hilo(comensal) %d , me voy a comer una hamburguesa ! ya que todavia queda/n %d \n",
-			 (int) tid, cantidad_restante_hamburguesas);
+			printf("Hola! soy el hilo(comensal) %d , me voy a comer una hamburguesa ! ya que todavia queda/n %d \n", (int) tid, cantidad_restante_hamburguesas);
 			cantidad_restante_hamburguesas--; // me como una hamburguesa
 		}
 		else
 		{
 			printf("SE TERMINARON LAS HAMBURGUESAS :( \n");
-
+			turno = (turno + 1)% NUMBER_OF_THREADS;
 			pthread_exit(NULL); // forzar terminacion del hilo
 		}
-    // SALIDA DE LA ZONA CRÍTICA 
-	turno = (turno + 1)% NUMBER_OF_THREADS;  
-}
+    // SALIDA DE LA ZONA CRÍTICA   
+			
+			turno = (turno + 1)% NUMBER_OF_THREADS;
+	}
 	}
 }
 
@@ -34,7 +35,6 @@ int main(int argc, char *argv[])
 {
 	pthread_t threads[NUMBER_OF_THREADS];
 	int status, i, ret;
-	int turno = 0;
 	for (int i = 0; i < NUMBER_OF_THREADS; i++)
 	{
 		printf("Hola!, soy el hilo principal. Estoy creando el hilo %d \n", i);
